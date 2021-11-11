a5fa081ab3abd499e8e8e821076722abdd9f3c56
BunnY <BunnY@LAPTOP-MAB5PI77>
2021-11-11 12:31:46 +0530

    this is my first try

---
+++ src/main/java/Testing/Test1.java
@@ -0,0 +1,11 @@
+package Testing;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void method1()
+{
+	System.out.println("heyyyy");
+	}
+}
---
+++ src/main/java/Testing/Test2.java
@@ -0,0 +1,11 @@
+package Testing;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void method2()
+{
+	System.out.println("heyy again");
+	}
+}
